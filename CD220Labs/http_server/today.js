a1064baa66e6d838f01fccfaab525ff0d82b6c9e
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-08-04 19:12:13 -0400

    Updated http_server modules

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,13 @@
- // Export a function named 'getDate' from the module
+// Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
-    // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+  // Get the current date and time in the timezone "Australia/Brisbane"
+  let time = new Date().toLocaleString("en-US", {
+    timeZone: "America/Detroit",
+  }); //, {timeZone: "Australia/Brisbane"}
+  return time; // Return the formatted date and time
+};
+
+module.exports.getHour = function getHour() {
+  let hour = new Date().getHours();
+  return hour;
 };
